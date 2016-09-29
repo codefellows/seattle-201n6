1983b06920b9d26a6b0d8e0340127e72221e5029
Dan Schwartz <dpschwartz@gmail.com>
2016-09-28 21:10:41 -0700

    Add finished constructor/table demo

--- class-07-oop--constructors--tables/lecture/app.js
+++ class-07-oop--constructors--tables/lecture/app.js
@@ -1,69 +1,80 @@
-var socks = {
-  name: 'socks',
-  price: 8.99,
-  tax: 0,
-  total: 0,
+'use strict';
 
-  calcTax: function(price) {
-    return parseFloat((price * 0.095).toFixed(2));
-  },
-  calcTotal: function() {
-    this.total = parseFloat((this.price + this.tax).toFixed(2));
-  }
+function round(num, prec) {
+  return parseFloat(num.toFixed(prec));
+}
+
+var allItems = [];
+
+function Item(name, price) {
+  this.name = name;
+  this.price = price;
+  this.tax = 0;
+  this.total = 0;
+  allItems.push(this);
+}
+
+Item.prototype.calcTax = function (price) {
+  this.tax = round((price * 0.095), 2);
 };
 
-socks.tax = socks.calcTax(socks.price);
-socks.calcTotal();
-
-var shoes = {
-  name: 'shoes',
-  price: 49.99,
-  tax: 0,
-  total: 0,
-
-  calcTax: function(price) {
-    return parseFloat((price * 0.095).toFixed(2));
-  },
-  calcTotal: function() {
-    this.total = parseFloat((this.price + this.tax).toFixed(2));
-  }
+Item.prototype.calcTotal = function() {
+  this.total = round((this.price + this.tax), 2);
 };
 
-shoes.tax = shoes.calcTax(shoes.price);
-shoes.calcTotal();
-
-
-var pantaloons = {
-  name: 'pantaloons',
-  price: 89.99,
-  tax: 0,
-  total: 0,
-
-  calcTax: function(price) {
-    return parseFloat((price * 0.095).toFixed(2));
-  },
-  calcTotal: function() {
-    this.total = parseFloat((this.price + this.tax).toFixed(2));
-  }
+Item.prototype.doAllTheMethods = function () {
+  this.calcTax(this.price);
+  this.calcTotal();
 };
 
-pantaloons.tax = pantaloons.calcTax(pantaloons.price);
-pantaloons.calcTotal();
+new Item('socks', 8.99);
+new Item('shoes', 49.99);
+new Item('pantaloons', 89.99);
 
+function makeAllTheThings() {
+  for (var i = 0; i < allItems.length; i++) {
+    allItems[i].doAllTheMethods();
+  }
+}
+
+makeAllTheThings();
 
 //get reference to table element
+var tableEl = document.getElementById('generated-table');
 
 function makeRow(obj) {
   //make a row
+  var rowEl = document.createElement('tr');
 
   //REPEAT THIS PART
     //make a cell
-
+  var nameCell = document.createElement('td');
     //give content to cell
-
+  nameCell.textContent = obj.name;
     //append cell to the row
+  rowEl.appendChild(nameCell);
 
+  var priceCell = document.createElement('td');
+  priceCell.textContent = obj.price;
+  rowEl.appendChild(priceCell);
+
+  var taxEl = document.createElement('td');
+  taxEl.textContent = obj.tax;
+  rowEl.appendChild(taxEl);
+
+  var totalEl = document.createElement('td');
+  totalEl.textContent = obj.total;
+  rowEl.appendChild(totalEl);
 
   //append row to the table
-
+  tableEl.appendChild(rowEl);
 }
+
+function makeTable() {
+  for (var item of allItems) {
+
+    makeRow(item);
+  }
+}
+
+makeTable();
